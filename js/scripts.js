f657ffdebb0ff7377d4ffd39ab3895ea0a67952d
JasonMDev <jasonmansdev@gmail.com>
2015-12-29 21:20:33 +0000

    A2#6: Added js fie and moved the javascript to it.

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// Twitter script
+!function(d,s,id) {
+	var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+	if(!d.getElementById(id)){
+		js=d.createElement(s);
+    js.id=id;
+    js.src=p+'://platform.twitter.com/widgets.js';
+    fjs.parentNode.insertBefore(js,fjs);
+    }
+  } (document, 'script', 'twitter-wjs');
+
+// Facebook Script
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+  }( document, 'script', 'facebook-jssdk'));
+
+
+$(document).ready(function() {
+	// Code goes here.
+	$("#message-box").css("border","2px solid red");
+
+	//Stellar.js for paralax effect  '.paralax-section'
+  $(window).stellar(); 
+  
+  //Tooltip script via "bootstrap.min.js" targeting single id.
+  $(function () {
+  	$('#item1').tooltip();
+  	});
+
+  //Tooltip script via "bootstrap.min.js" targeting class button.
+  $(function () {
+  	$('[data-toggle="tooltip"]').tooltip();
+  	}); 
+  
+  //Creates smooth scrolling effect for menus.
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+  	var href = $.attr(this, 'href');
+    $root.animate({
+    	scrollTop: $(href).offset().top
+    	}, 500, function () {
+    		window.location.hash = href;
+    		});
+    return false;
+    });
+
+});
